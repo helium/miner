b6e0c635bc318749a8e81cc138c994b76cf81478
Rahul Garg <vihu89@gmail.com>
2019-03-05 11:25:39 -0800

    Test self_payment is an invalid txn

--- test/miner_payment_txn_SUITE.erl
+++ test/miner_payment_txn_SUITE.erl
@@ -5,21 +5,23 @@
 -include_lib("kernel/include/inet.hrl").
 
 -export([
-         init_per_suite/1
-         ,end_per_suite/1
-         ,init_per_testcase/2
-         ,end_per_testcase/2
-         ,all/0
+         init_per_suite/1,
+         end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
+         all/0
         ]).
 
 -export([
-         single_payment_test/1
+         single_payment_test/1,
+         self_payment_test/1
         ]).
 
 %% common test callbacks
 
 all() -> [
-          single_payment_test
+          single_payment_test,
+          self_payment_test
          ].
 
 init_per_suite(Config) ->
@@ -130,7 +132,65 @@ single_payment_test(Config) ->
     ct:comment("FinalPayerBalance: ~p, FinalPayeeBalance: ~p", [PayerBalance, PayeeBalance]),
     ok.
 
+self_payment_test(Config) ->
+    Miners = proplists:get_value(miners, Config),
+    [Payer, Payee | _Tail] = Miners,
+    PayerAddr = ct_rpc:call(Payer, blockchain_swarm, pubkey_bin, []),
+    PayeeAddr = PayerAddr,
 
+    %% check initial balances
+    %% FIXME: really need to be setting the balances elsewhere
+    5000 = get_balance(Payer, PayerAddr),
+    5000 = get_balance(Payee, PayerAddr),
+
+    Chain = ct_rpc:call(Payer, blockchain_worker, blockchain, []),
+    Ledger = ct_rpc:call(Payer, blockchain, ledger, [Chain]),
+
+    {ok, Fee} = ct_rpc:call(Payer, blockchain_ledger_v1, transaction_fee, [Ledger]),
+
+    %% send some helium tokens from payer to payee
+    Txn = ct_rpc:call(Payer, blockchain_txn_payment_v1, new, [PayerAddr, PayeeAddr, 1000, Fee, 1]),
+
+    {ok, _Pubkey, SigFun} = ct_rpc:call(Payer, blockchain_swarm, keys, []),
+
+    SignedTxn = ct_rpc:call(Payer, blockchain_txn_payment_v1, sign, [Txn, SigFun]),
+
+    ok = ct_rpc:call(Payer, blockchain_worker, submit_txn, [SignedTxn]),
+
+    %% XXX: presumably the transaction wouldn't have made it to the blockchain yet
+    %% get the current height here
+    Chain2 = ct_rpc:call(Payer, blockchain_worker, blockchain, []),
+    {ok, CurrentHeight} = ct_rpc:call(Payer, blockchain, height, [Chain2]),
+
+    %% XXX: wait till the blockchain grows by 2 blocks
+    %% assuming that the transaction makes it within 2 blocks
+    ok = miner_ct_utils:wait_until(
+           fun() ->
+                   true =:= lists:all(
+                              fun(Miner) ->
+                                      C = ct_rpc:call(Miner, blockchain_worker, blockchain, []),
+                                      {ok, Height} = ct_rpc:call(Miner, blockchain, height, [C]),
+                                      Height >= CurrentHeight + 2
+                              end,
+                              Miners
+                             )
+           end,
+           60,
+           timer:seconds(1)
+          ),
+
+    PayerBalance = get_balance(Payer, PayerAddr),
+    PayeeBalance = get_balance(Payee, PayeeAddr),
+
+    %% No change in balances since the payment should have failed, fee=0 anyway
+    5000 = PayerBalance + Fee,
+    5000 = PayeeBalance,
+
+    ct:comment("FinalPayerBalance: ~p, FinalPayeeBalance: ~p", [PayerBalance, PayeeBalance]),
+    ok.
+
+
+%% Helper functions
 get_balance(Miner, Addr) ->
     Chain = ct_rpc:call(Miner, blockchain_worker, blockchain, []),
     Ledger = ct_rpc:call(Miner, blockchain, ledger, [Chain]),
