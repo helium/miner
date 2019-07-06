032ce07f21155e3f0ac49f19d0901249479756fd
Andrew Thompson <andrew@hijacked.us>
2019-07-05 18:06:32 -0700

    Add more entropy to consensus group names

--- src/miner_consensus_mgr.erl
+++ src/miner_consensus_mgr.erl
@@ -164,7 +164,8 @@ handle_call({genesis_block_done, BinaryGenesisBlock, Signatures, Members, PrivKe
                                       Chain,
                                       1,
                                       []]],
-    {ok, Group} = libp2p_swarm:add_group(blockchain_swarm:swarm(), "consensus_1",
+    Name = "consensus_1_"++integer_to_list(erlang:phash2(Members)),
+    {ok, Group} = libp2p_swarm:add_group(blockchain_swarm:swarm(), Name,
                                          libp2p_group_relcast, GroupArg),
     lager:info("started initial hbbft group: ~p~n", [Group]),
     %% NOTE: I *think* this is the only place to store the chain reference in the miner state
@@ -212,7 +213,7 @@ handle_call({election_done, _Artifact, Signatures, Members, PrivKey}, _From,
                                       1, % gets set later
                                       []]], % gets filled later
     %% while this won't reflect the actual height, it has to be deterministic
-    Name = "consensus_" ++ integer_to_list(max(0, Height)),
+    Name = "consensus_" ++ integer_to_list(max(0, Height)) ++"_"++integer_to_list(erlang:phash2(Members)),
     {ok, Group} = libp2p_swarm:add_group(blockchain_swarm:swarm(),
                                          Name,
                                          libp2p_group_relcast, GroupArg),
@@ -246,7 +247,7 @@ handle_call({maybe_start_consensus_group, StartHeight}, _From,
                                                       undefined,
                                                       Chain]],
                     %% while this won't reflect the actual height, it has to be deterministic
-                    Name = "consensus_" ++ integer_to_list(StartHeight),
+                    Name = "consensus_" ++ integer_to_list(StartHeight) ++"_"++integer_to_list(erlang:phash2(ConsensusAddrs)),
                     {ok, Group} = libp2p_swarm:add_group(blockchain_swarm:swarm(),
                                                          Name,
                                                          libp2p_group_relcast, GroupArg),
