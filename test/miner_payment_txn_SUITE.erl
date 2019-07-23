5f7788f1a3f1b87a7402a767daf502ded34103e6
Rahul Garg <vihu89@gmail.com>
2019-07-22 21:03:46 -0700

    Remove unused chain vars

--- test/miner_payment_txn_SUITE.erl
+++ test/miner_payment_txn_SUITE.erl
@@ -69,8 +69,6 @@ init_per_testcase(_TestCase, Config0) ->
              election_replacement_factor => 4,
              election_replacement_slope => 20,
              min_score => 0.2,
-             h3_ring_size => 2,
-             h3_path_res => 8,
              alpha_decay => 0.007,
              beta_decay => 0.0005,
              max_staleness => 100000,
