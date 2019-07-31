e518a1d0c80c94b22c0ee46c7f0c0dd15852add3
Evan Vigil-McClanahan <evan@helium.com>
2019-07-31 09:01:06 -0700

    basic message minimization

--- src/miner_ecc_worker.erl
+++ src/miner_ecc_worker.erl
@@ -9,11 +9,14 @@
          init/1,
          handle_call/3,
          handle_cast/2,
+         handle_info/2,
          terminate/2]).
 
 -record(state, {
                 ecc_handle :: pid(),
-                key_slot :: non_neg_integer()
+                key_slot :: non_neg_integer(),
+                idle_timer = make_ref() :: reference(),
+                awake = false
                }).
 
 -spec sign(binary()) -> {ok, Signature::binary()} | {error, term()}.
@@ -33,16 +36,14 @@ init([KeySlot]) ->
     {ok, #state{ecc_handle=ECCHandle, key_slot=KeySlot}}.
 
 
-handle_call({sign, Binary}, _From, State=#state{ecc_handle=Pid}) ->
-    ecc508:wake(Pid),
+handle_call({sign, Binary}, _From, State) ->
+    {Pid, State1} = wake(State),
     Reply = ecc508:sign(Pid, State#state.key_slot, Binary),
-    ecc508:idle(Pid),
-    {reply, Reply, State};
+    {reply, Reply, idle_timeout(State1)};
 handle_call({ecdh, PubKey}, _From, State=#state{ecc_handle=Pid}) ->
-    ecc508:wake(Pid),
+    {Pid, State1} = wake(State),
     Reply = ecc508:ecdh(Pid, State#state.key_slot, PubKey),
-    ecc508:idle(Pid),
-    {reply, Reply, State};
+    {reply, Reply, idle_timeout(State1)};
 
 handle_call(_Msg, _From, State) ->
     lager:warning("unhandled call ~p", [_Msg]),
@@ -53,5 +54,24 @@ handle_cast(_Msg, State) ->
     lager:warning("unhandled cast ~p", [_Msg]),
     {noreply, State}.
 
-terminate(_Reason, State=#state{}) ->
+handle_info(idle_timeout, #state{ecc_handle = Pid} = State) ->
+    ecc508:idle(Pid),
+    {noreply, State#state{awake = false}};
+handle_info(_Msg, State) ->
+    lager:warning("unhandled msg ~p", [_Msg]),
+    {noreply, State}.
+
+terminate(_Reason, State) ->
     catch ecc508:stop(State#state.ecc_handle).
+
+wake(S = #state{awake = true, ecc_handle = Pid}) ->
+    {Pid, S};
+wake(S = #state{ecc_handle = Pid}) ->
+    ecc508:wake(Pid),
+    {Pid, S#state{awake = true}}.
+
+idle_timeout(#state{idle_timer = Ref} = S) ->
+    erlang:cancel_timer(Ref),
+    Timeout = application:get_env(miner, ecc_idle_timeout, 100),
+    Ref1 = erlang:send_after(Timeout, self(), idle_timeout),
+    S#state{idle_timer = Ref1}.
