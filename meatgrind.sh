983ea995d9af21ed44410cb849d36d144c69a153
Andrew Thompson <andrew@hijacked.us>
2019-08-15 15:38:29 -0700

    Add a meatgrinder script to randomly restart/kill run.sh nodes

---
+++ meatgrind.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+N=8
+N2=$(( $N * 2))
+while true; do
+    #RAND=`date +%N|sed s/...$//`
+    RAND=$RANDOM
+    ## sleep up to 20 seconds
+    sleep "$(( $RAND % 20))"
+    export I=$(expr $RAND % $N2 + 1)
+    ## 50 percent chance of stopping
+    ## 50 percent chance of kill -9
+    if [ $I -lt 9 ]; then
+        ./cmd.sh $I stop
+    else
+        export I=$(( $I - $N))
+        MURDERPID=$(./cmd.sh $I eval 'list_to_integer(os:getpid())')
+        kill -9 $MURDERPID
+    fi
+    while [ "$(./cmd.sh $I ping)" = "pong" ]; do
+        sleep 1
+    done
+    ## 66% chance of restarting things
+    if [ ! $(expr $RAND % 3) -eq 0 ]; then
+        ./cmd.sh $I start
+    fi
+done
