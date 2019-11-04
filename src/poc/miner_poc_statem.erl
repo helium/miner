9eaf4f5b80a65c70bbdf6b13261d6b05311f44ef
macpie <pierre@helium.com>
2019-11-04 10:44:54 -0800

    Do not allow more than one 1 witness per layer per hotspot

--- src/poc/miner_poc_statem.erl
+++ src/poc/miner_poc_statem.erl
@@ -278,12 +278,14 @@ receiving(cast, {witness, Witness}, #data{responses=Responses0,
                             {keep_state, Data};
                         false ->
                             %% Don't allow putting duplicate response in the witness list resp
-                            Responses1 = case lists:member(Witness, Witnesses) of
-                                             false ->
-                                                 maps:put(PacketHash, [Witness|Witnesses], Responses0);
-                                             true ->
-                                                 Responses0
-                                         end,
+                            Responses1 =
+                                Filter = fun(W) -> blockchain_poc_witness_v1:gateway(W) == GatewayWitness end,
+                                case lists:any(Filter, Witnesses) of
+                                    false ->
+                                        maps:put(PacketHash, [Witness|Witnesses], Responses0);
+                                    true ->
+                                        Responses0
+                                end,
                             {keep_state, save_data(Data#data{responses=Responses1})}
                     end
             end
