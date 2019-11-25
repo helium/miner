60decd93db5aef359464fde63339cdca8db94739
andymck <andrewb.mckenzie@gmail.com>
2019-11-25 17:12:42 +0000

    remove tmp debug output

--- test/miner_ct_macros.hrl
+++ test/miner_ct_macros.hrl
@@ -3,7 +3,6 @@
 
 -define(assertAsync(Expr, BoolExpr, ExprRetry, ExprDelay),
     Res = miner_ct_utils:wait_until(fun() -> (Expr),(BoolExpr) end, ExprRetry, ExprDelay),
-    ct:pal("Async Res: ~p for expr: ~p", [Res, ??BoolExpr]),
     case Res of
         false -> 
             erlang:error({assert,
