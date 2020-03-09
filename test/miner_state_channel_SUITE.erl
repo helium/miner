86e88d72fd7fa22d3051615fddfc3928228fed30
Rahul Garg <rg@vihu.dev>
2020-03-09 13:53:17 -0700

    Add state_channel test suite

---
+++ test/miner_state_channel_SUITE.erl
@@ -0,0 +1,158 @@
+-module(miner_state_channel_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/inet.hrl").
+-include_lib("blockchain/include/blockchain_vars.hrl").
+-include("miner_ct_macros.hrl").
+
+-export([
+         init_per_suite/1,
+         end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
+         all/0
+        ]).
+
+-export([
+         basic_test/1
+        ]).
+
+%% common test callbacks
+
+all() -> [
+          basic_test
+         ].
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(Config) ->
+    Config.
+
+init_per_testcase(_TestCase, Config0) ->
+    Config = miner_ct_utils:init_per_testcase(?MODULE, _TestCase, Config0),
+    Miners = ?config(miners, Config),
+    Addresses = ?config(addresses, Config),
+    Balance = 5000,
+    InitialPaymentTransactions = [ blockchain_txn_coinbase_v1:new(Addr, Balance) || Addr <- Addresses],
+    InitialDCTxns = [blockchain_txn_dc_coinbase_v1:new(Addr, Balance) || Addr <- Addresses],
+    AddGwTxns = [blockchain_txn_gen_gateway_v1:new(Addr, Addr, h3:from_geo({37.780586, -122.469470}, 13), 0)
+                 || Addr <- Addresses],
+
+    NumConsensusMembers = ?config(num_consensus_members, Config),
+    BlockTime = ?config(block_time, Config),
+    BatchSize = ?config(batch_size, Config),
+    Curve = ?config(dkg_curve, Config),
+    %% VarCommitInterval = ?config(var_commit_interval, Config),
+
+    Keys = libp2p_crypto:generate_keys(ecc_compact),
+
+    InitialVars = miner_ct_utils:make_vars(Keys, #{?block_time => BlockTime,
+                                                   %% rule out rewards
+                                                   ?election_interval => infinity,
+                                                   ?num_consensus_members => NumConsensusMembers,
+                                                   ?batch_size => BatchSize,
+                                                   ?dkg_curve => Curve,
+                                                   ?max_payments => 10}),
+
+    DKGResults = miner_ct_utils:inital_dkg(Miners,
+                                           InitialVars ++ InitialPaymentTransactions ++ AddGwTxns ++ InitialDCTxns,
+                                           Addresses, NumConsensusMembers, Curve),
+    true = lists:all(fun(Res) -> Res == ok end, DKGResults),
+
+    %% Get both consensus and non consensus miners
+    {ConsensusMiners, NonConsensusMiners} = miner_ct_utils:miners_by_consensus_state(Miners),
+    %% integrate genesis block
+    _GenesisLoadResults = miner_ct_utils:integrate_genesis_block(hd(ConsensusMiners), NonConsensusMiners),
+
+    %% confirm we have a height of 1
+    ok = miner_ct_utils:wait_for_gte(height_exactly, Miners, 1),
+
+    [   {consensus_miners, ConsensusMiners},
+        {non_consensus_miners, NonConsensusMiners}
+        | Config].
+
+
+end_per_testcase(_TestCase, Config) ->
+    miner_ct_utils:end_per_testcase(_TestCase, Config).
+
+basic_test(Config) ->
+    Miners = ?config(miners, Config),
+
+    [RouterNode | _] = Miners,
+
+    %% setup
+    %% oui txn
+    {ok, RouterPubkey, RouterSigFun, _ECDHFun} = ct_rpc:call(RouterNode, blockchain_swarm, keys, []),
+    RouterPubkeyBin = libp2p_crypto:pubkey_to_bin(RouterPubkey),
+    RouterSwarm = ct_rpc:call(RouterNode, blockchain_swarm, swarm, []),
+    ct:pal("RouterSwarm: ~p", [RouterSwarm]),
+    RouterP2PAddress = ct_rpc:call(RouterNode, libp2p_swarm, p2p_address, [RouterSwarm]),
+    ct:pal("RouterP2PAddress: ~p", [RouterP2PAddress]),
+    OUITxn = ct_rpc:call(RouterNode,
+                         blockchain_txn_oui_v1,
+                         new,
+                         [RouterPubkeyBin, [erlang:list_to_binary(RouterP2PAddress)], 1, 1, 0]),
+    ct:pal("OUITxn: ~p", [OUITxn]),
+    SignedOUITxn = ct_rpc:call(RouterNode,
+                               blockchain_txn_oui_v1,
+                               sign,
+                               [OUITxn, RouterSigFun]),
+    ct:pal("SignedOUITxn: ~p", [SignedOUITxn]),
+    ok = ct_rpc:call(RouterNode, blockchain_worker, submit_txn, [SignedOUITxn]),
+
+    %% check that oui txn appears on miners
+    CheckTypeOUI = fun(T) -> blockchain_txn:type(T) == blockchain_txn_oui_v1 end,
+    CheckTxnOUI = fun(T) -> T == SignedOUITxn end,
+    ok = wait_for_txn(Miners, CheckTypeOUI, timer:seconds(30)),
+    ok = wait_for_txn(Miners, CheckTxnOUI, timer:seconds(30)),
+
+    %% open a state channel
+    TotalDC = 10,
+    ID = crypto:strong_rand_bytes(32),
+    SCOpenTxn = ct_rpc:call(RouterNode,
+                            blockchain_txn_state_channel_open_v1,
+                            new,
+                            [ID, RouterPubkeyBin, TotalDC, 10, 1]),
+    ct:pal("SCOpenTxn: ~p", [SCOpenTxn]),
+    SignedSCOpenTxn = ct_rpc:call(RouterNode,
+                                  blockchain_txn_state_channel_open_v1,
+                                  sign,
+                                  [SCOpenTxn, RouterSigFun]),
+    ct:pal("SignedSCOpenTxn: ~p", [SignedSCOpenTxn]),
+    ok = ct_rpc:call(RouterNode, blockchain_worker, submit_txn, [SignedSCOpenTxn]),
+
+    %% check that sc open txn appears on miners
+    CheckTypeSCOpen = fun(T) -> blockchain_txn:type(T) == blockchain_txn_state_channel_open_v1 end,
+    CheckTxnSCOpen = fun(T) -> T == SignedSCOpenTxn end,
+    ok = wait_for_txn(Miners, CheckTypeSCOpen, timer:seconds(30)),
+    ok = wait_for_txn(Miners, CheckTxnSCOpen, timer:seconds(30)),
+
+    ok.
+
+wait_for_txn(Miners, PredFun, Timeout)->
+    ?assertAsync(begin
+                     Result = lists:all(
+                                fun(Miner) ->
+                                        C = ct_rpc:call(Miner, blockchain_worker, blockchain, [], Timeout),
+                                        {ok, H} = ct_rpc:call(Miner, blockchain, height, [C], Timeout),
+                                        Blocks = ct_rpc:call(Miner, blockchain, blocks, [C], Timeout),
+
+                                        Res = lists:filter(fun({_Hash, Block}) ->
+                                                                   BH = blockchain_block:height(Block),
+                                                                   Txns = blockchain_block:transactions(Block),
+                                                                   ToFind = lists:filter(fun(T) ->
+                                                                                                 PredFun(T)
+                                                                                         end,
+                                                                                         Txns),
+                                                                   ct:pal("BlockHeight: ~p, ToFind: ~p", [BH, ToFind]),
+                                                                   ToFind /= []
+                                                           end,
+                                                           maps:to_list(Blocks)),
+                                        ct:pal("ChainHeight: ~p, Res: ~p", [H, Res]),
+                                        Res /= []
+                                end, miner_ct_utils:shuffle(Miners))
+                 end,
+                 Result == true, 40, timer:seconds(1)),
+    ok.
