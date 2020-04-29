6259c4715fc482830363272571f9cd70ed5ce0a8
Mark Allen <mrallen1@yahoo.com>
2020-04-29 14:29:19 -0500

    Mount qemu in Docker build

--- .buildkite/scripts/make_docker.sh
+++ .buildkite/scripts/make_docker.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 MINER_REGISTRY_NAME="quay.io/team-helium/miner"
 DOCKER_NAME="$(basename $(pwd))_${BUILDKITE_TAG}"
 
-docker build -t helium:$DOCKER_NAME -f .buildkite/scripts/Dockerfile .
+docker build \
+    --volume type=bind,source=/usr/bin/qemu-arm-static,target=/usr/bin/qemu-arm-static \
+    -t helium:$DOCKER_NAME -f .buildkite/scripts/Dockerfile .
+
 docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:$DOCKER_NAME"
 
 docker login -u="team-helium+buildkite" -p="QJQP7M6WWE00QJOTUIEBF8N16F4THA9O2MF6QBM1ZGGVK33TG2VLCGK50SUHSWED" quay.io
