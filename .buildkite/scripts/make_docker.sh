cf948dde6ade42f82294676d94bbb8bb9c4a6460
Mark Allen <mrallen1@yahoo.com>
2020-04-29 11:22:42 -0500

    Fix up docker build

--- .buildkite/scripts/make_docker.sh
+++ .buildkite/scripts/make_docker.sh
@@ -3,8 +3,7 @@ et -euo pipefail
 MINER_REGISTRY_NAME="quay.io/team-helium/miner"
 DOCKER_NAME="$(basename $(pwd))_${BUILDKITE_TAG}"
 
-docker build -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static:ro \
-             -t helium:$DOCKER_NAME -f .buildkite/scripts/Dockerfile .
+docker build -t helium:$DOCKER_NAME -f .buildkite/scripts/Dockerfile .
 docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:$DOCKER_NAME"
 
 docker login -u="team-helium+buildkite" -p="QJQP7M6WWE00QJOTUIEBF8N16F4THA9O2MF6QBM1ZGGVK33TG2VLCGK50SUHSWED" quay.io
