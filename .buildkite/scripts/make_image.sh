fa2eff32a0f77e06af73cb795a565208a68c8840
Mark Allen <mrallen1@yahoo.com>
2020-05-07 18:13:02 -0500

    Clean up scripts, add OCI build and export

---
+++ .buildkite/scripts/make_image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# MINER_REGISTRY_NAME
+# IMAGE_ARCH
+# IMAGE_FORMAT
+# all come from pipeline.yml
+
+DOCKER_NAME="$(basename $(pwd))-{$IMAGE_ARCH}_${BUILDKITE_TAG}"
+DOCKERFILE_NAME=".buildkite/scripts/Dockerfile-${IMAGE_ARCH}"
+TARNAME="${DOCKER_NAME}-oci.tar"
+
+docker login -u="team-helium+buildkite" -p="${QUAY_BUILDKITE_PASSWORD}"
+
+if [ "$IMAGE_FORMAT" -eq "oci" ]; then
+    docker buildx build -o type=oci,dest="${TARNAME}" -f "${DOCKERFILE_NAME}" .
+    docker import "${TARNAME}" "$MINER_REGISTRY_NAME:$DOCKER_NAME"
+else
+    docker build -t helium:$DOCKER_NAME -f "${DOCKERFILE_NAME}" .
+    docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:$DOCKER_NAME"
+fi
+
+docker push "$MINER_REGISTRY_NAME:$DOCKER_NAME"
