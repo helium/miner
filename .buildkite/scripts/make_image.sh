49c14861614f16dd442683f64e9f87e3775e50ae
Mark Allen <mrallen1@yahoo.com>
2020-05-08 13:40:13 -0500

    Derp

--- .buildkite/scripts/make_image.sh
+++ .buildkite/scripts/make_image.sh
@@ -8,13 +8,13 @@ set -euo pipefail
 # all come from pipeline.yml
 
 MINER_REGISTRY_NAME="$REGISTRY_HOST/team-helium/$REGISTRY_NAME"
-DOCKER_NAME="$(basename $(pwd))-{$IMAGE_ARCH}_${BUILDKITE_TAG}"
+DOCKER_NAME="$(basename $(pwd))-${IMAGE_ARCH}_${BUILDKITE_TAG}"
 DOCKERFILE_NAME=".buildkite/scripts/Dockerfile-${IMAGE_ARCH}"
 TARNAME="${DOCKER_NAME}-oci.tar"
 
 docker login -u="team-helium+buildkite" -p="${QUAY_BUILDKITE_PASSWORD}" ${REGISTRY_HOST}
 
-if [ "$IMAGE_FORMAT" -eq "oci" ]; then
+if [ "$IMAGE_FORMAT" = "oci" ]; then
     docker buildx build -o type=oci,dest="${TARNAME}" -f "${DOCKERFILE_NAME}" .
     docker import "${TARNAME}" "$MINER_REGISTRY_NAME:$DOCKER_NAME"
 else
