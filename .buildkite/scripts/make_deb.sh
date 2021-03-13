e641b45a0192b7959e9cbda745da006ebc4b7057
Evan Vigil-McClanahan <evan@helium.com>
2021-03-13 08:52:05 -0800

    fix tag

--- .buildkite/scripts/make_deb.sh
+++ .buildkite/scripts/make_deb.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 fpm -n validator \
-    -v $(git describe --long --always) \
+    -v $(git describe --abbrev=0 --tags | sed -e s/val//) \
     -s dir \
     -t deb \
     --depends libssl1.1 \
