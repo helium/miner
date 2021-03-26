f7679711db733658e57adeb96695a1782d49a938
Evan Vigil-McClanahan <evan@helium.com>
2021-03-25 22:03:31 -0700

    bump version

--- src/miner.erl
+++ src/miner.erl
@@ -320,7 +320,7 @@ remove_consensus() ->
 version() ->
     %% format:
     %% MMMmmmPPPP
-       0000010029.
+       0000010030.
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
