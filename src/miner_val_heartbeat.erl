e2ca4affb6d41a152274ebd6ad559b9600304a19
Evan Vigil-McClanahan <evan@helium.com>
2021-06-14 20:44:34 -0700

    be more aggressive about heartbeats

--- src/miner_val_heartbeat.erl
+++ src/miner_val_heartbeat.erl
@@ -13,13 +13,14 @@
          terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
+-define(start_wait, 5).
 
 -record(state,
         {
          address :: libp2p_crypto:address(),
          sigfun :: libp2p_crypto:sig_fun(),
          txn_status = ready :: ready | waiting,
-         txn_wait = 10 :: non_neg_integer(),
+         txn_wait = ?start_wait :: non_neg_integer(),
          chain :: undefined | blockchain:blockchain()
         }).
 
@@ -62,7 +63,7 @@ handle_info({blockchain_event, {add_block, _Hash, _Sync, _Ledger}},
             #state{txn_status = waiting, txn_wait = Wait} = State) ->
     case Wait of
         1 ->
-            {noreply, State#state{txn_status = ready, txn_wait = 10}};
+            {noreply, State#state{txn_status = ready, txn_wait = ?start_wait}};
         N ->
             {noreply, State#state{txn_wait = N - 1}}
     end;
@@ -72,14 +73,17 @@ handle_info({blockchain_event, {add_block, Hash, Sync, _Ledger}},
     case blockchain:config(?election_version, Ledger) of
         {ok, V} when V >= 5 ->
             {ok, HBInterval} = blockchain:config(?validator_liveness_interval, Ledger),
+            Now = erlang:system_time(seconds),
             {ok, Block} = blockchain:get_block(Hash, State#state.chain),
             Height = blockchain_block:height(Block),
+            TimeAgo = Now - blockchain_block:time(Block),
             %% heartbeat server needs to be able to run on an unstaked validator
             case blockchain_ledger_v1:get_validator(Address, Ledger) of
                 {ok, Val} ->
                     lager:debug("getting validator for address ~p got ~p", [Address, Val]),
                     case blockchain_ledger_validator_v1:last_heartbeat(Val) of
-                        N when (N + HBInterval) =< Height andalso (not Sync) ->
+                        N when (N + HBInterval) =< Height
+                               andalso ((not Sync) orelse TimeAgo =< (60 * 30)) ->
                             %% we need to construct and submit a heartbeat txn
                             {ok, CBMod} = blockchain_ledger_v1:config(?predicate_callback_mod, Ledger),
                             {ok, Callback} = blockchain_ledger_v1:config(?predicate_callback_fun, Ledger),
