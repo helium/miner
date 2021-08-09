3b49cfab81a70389fcd1f5a5ced826ae21708f92
Mark Allen <mrallen1@yahoo.com>
2021-08-09 12:50:48 -0500

    Support seed debian builds

--- .buildkite/scripts/packagecloud_upload.sh
+++ .buildkite/scripts/packagecloud_upload.sh
@@ -2,13 +2,20 @@
 
 set -euo pipefail
 
-TAG=$( git describe --abbrev=0 --tags | sed -e s/validator// )
+TAG=$( git describe --abbrev=0 --tags | sed -e s/$1// )
 
-PKGNAME="validator_${TAG}_amd64.deb"
+PKGNAME="$1_${TAG}_amd64.deb"
 
 buildkite-agent artifact download ${PKGNAME} .
+
+if [[ "$1" == "validator" ]]; then
+    # it's _validatorS_ in packagecloud
+    REPO="$1s"
+else
+    REPO="$1"
+fi
 
 curl -u "${PACKAGECLOUD_API_KEY}:" \
      -F "package[distro_version_id]=190" \
      -F "package[package_file]=@${PKGNAME}" \
-     https://packagecloud.io/api/v1/repos/helium/validators/packages.json
+     "https://packagecloud.io/api/v1/repos/helium/${REPO}/packages.json"
