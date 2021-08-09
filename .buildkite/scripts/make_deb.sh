70d2b45222af2b6670c80fa1fc4449188ae0fedd
Mark Allen <mrallen1@yahoo.com>
2021-08-09 12:50:48 -0500

    parent c8aa3b52d5a977d7c79e60fa4bfe3a0c3a89be3e author Mark Allen <mrallen1@yahoo.com> 1628531448 -0500 committer Jade Allen <jadeallenx@outlook.com> 1633389575 -0500
    Support seed debian builds
    
    pick up vm.args drop system_lib exclude
    
    Increase number of TCP acceptor processes from 10 to 50

--- .buildkite/scripts/make_deb.sh
+++ .buildkite/scripts/make_deb.sh
@@ -2,15 +2,23 @@
 
 set -euo pipefail
 
-VERSION=$(git describe --abbrev=0 | sed -e 's,validator,,')
-./rebar3 as validator release -n miner -v ${VERSION}
+TAG=$( git describe --abbrev=0 --tags | sed -e s/$1// )
 
-fpm -n validator \
-    -v "${VERSION}" \
+./rebar3 as $1 release -v $TAG -n miner
+
+fpm -n $1 \
+    -v "${TAG}" \
     -s dir \
     -t deb \
     --depends libssl1.1 \
     --depends libsodium23 \
+    --depends libc6 \
+    --depends libncurses5 \
+    --depends libgcc1 \
+    --depends libstdc++6 \
+    --depends libwxbase3.0-0v5 \
+    --depends libwxgtk3.0-gtk3-0v5 \
+    --depends libsctp1 \
     --deb-systemd deb/miner.service \
     --deb-no-default-config-files \
-    _build/validator/rel/=/var/helium
+    _build/$1/rel/=/var/helium
