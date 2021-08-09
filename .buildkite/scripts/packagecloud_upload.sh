106d5858eaf5e3e528fd0bc7cef4bac627aac5a0
Jade Allen <jadeallenx@outlook.com>
2021-08-09 12:50:48 -0500

    seed build
    Support seed debian builds
    
    pick up vm.args drop system_lib exclude
    
    Increase number of TCP acceptor processes from 10 to 50

--- .buildkite/scripts/packagecloud_upload.sh
+++ .buildkite/scripts/packagecloud_upload.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-TAG=$( git describe --abbrev=0 --tags | sed -e s/validator// )
+TAG=$( git describe --abbrev=0 --tags | sed -e s/$1// )
+DEBNAME=$( echo $1 | sed -e s/_/-/ )
 
-PKGNAME="validator_${TAG}_amd64.deb"
+PKGNAME=$"${DEBNAME}_${TAG}_amd64.deb"
 
 buildkite-agent artifact download ${PKGNAME} .
+
+# packagecloud name is testnet_seedS, seedS or validatorS
+REPO="$1s"
 
 curl -u "${PACKAGECLOUD_API_KEY}:" \
      -F "package[distro_version_id]=190" \
      -F "package[package_file]=@${PKGNAME}" \
-     https://packagecloud.io/api/v1/repos/helium/validators/packages.json
+     "https://packagecloud.io/api/v1/repos/helium/${REPO}/packages.json"
