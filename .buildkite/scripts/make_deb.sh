2feeb49aef294741ba0565079e26aa4e72c3b49b
Marc Nijdam <marc_nijdam@mac.com>
2021-08-13 14:40:59 -0400

    Attempts to add the needed dependencies
    This should satisfy the list of dependencies needed by bundled erts for erlang 22

--- .buildkite/scripts/make_deb.sh
+++ .buildkite/scripts/make_deb.sh
@@ -10,6 +10,13 @@ fpm -n $1 \
     -t deb \
     --depends libssl1.1 \
     --depends libsodium23 \
+    --depends libc6 \
+    --depends libncurses5 \
+    --depends libgcc1 \
+    --depends libstdc++6 \
+    --depends libwxbase3.0-0v5 \
+    --depends libwxgtk3.0-gtk3-0v5 \
+    --depends libsctp1 \
     --deb-systemd deb/miner.service \
     --deb-no-default-config-files \
     _build/$1/rel/=/var/helium
