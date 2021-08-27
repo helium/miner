24f6eb5431323264cbe858261eca0375186f5ad0
Jade Allen <jadeallenx@outlook.com>
2021-08-27 15:50:08 -0500

    Unify Docker builds into one file/script driver

--- .buildkite/scripts/make_image.sh
+++ .buildkite/scripts/make_image.sh
@@ -2,21 +2,77 @@
 
 set -euo pipefail
 
-# MINER_REGISTRY_NAME
+# REGISTRY_HOST
+# REGISTRY_NAME
 # IMAGE_ARCH
-# IMAGE_FORMAT
+# BUILD_TYPE
 # all come from pipeline.yml
 
+ERLANG_IMAGE="23.3.4.5-alpine"
+ERLANG_IMAGE_SOURCE="erlang"
+
 MINER_REGISTRY_NAME="$REGISTRY_HOST/team-helium/$REGISTRY_NAME"
-DOCKER_NAME="$(basename $(pwd))-${IMAGE_ARCH}_${BUILDKITE_TAG}"
-DOCKERFILE_NAME=".buildkite/scripts/Dockerfile-${IMAGE_ARCH}"
+DOCKER_NAME="$(basename $(pwd))"
+
+VERSION=$(git describe --abbrev=0 | sed -e "s/${BUILD_TYPE}//")
+DOCKER_BUILD_ARGS="--build-arg VERSION=${VERSION}"
+
+LATEST_TAG="$MINER_REGISTRY_NAME:latest-${IMAGE_ARCH}"
+
+case "$BUILD_TYPE" in
+    "val")
+        echo "Doing a testnet validator image build for ${IMAGE_ARCH}"
+        if [[ "$IMAGE_ARCH" == "arm64" ]]; then
+            ERLANG_IMAGE_SOURCE="arm64v8"
+        fi
+        BASE_IMAGE="${ERLANG_IMAGE_SOURCE}:${ERLANG_IMAGE}"
+        DOCKER_BUILD_ARGS="--build-arg BUILDER_IMAGE=${BASE_IMAGE} --build-arg RUNNER_IMAGE=${BASE_IMAGE} --build-arg REBAR_BUILD_TARGET=docker_testval ${DOCKER_BUILD_ARGS}"
+        DOCKER_NAME="${DOCKER_NAME}-${IMAGE_ARCH}_testnet_${BUILDKITE_TAG}"
+        LATEST_TAG="$MINER_REGISTRY_NAME:latest-val-${IMAGE_ARCH}"
+        ;;
+    "validator")
+        echo "Doing a mainnet validator image build for $IMAGE_ARCH"
+        if [[ "$IMAGE_ARCH" == "arm64" ]]; then
+            ERLANG_IMAGE_SOURCE="arm64v8"
+        fi
+        BASE_IMAGE="${ERLANG_IMAGE_SOURCE}:${ERLANG_IMAGE}"
+        DOCKER_BUILD_ARGS="--build-arg BUILDER_IMAGE=${BASE_IMAGE} --build-arg RUNNER_IMAGE=${BASE_IMAGE} --build-arg REBAR_BUILD_TARGET=docker_val ${DOCKER_BUILD_ARGS}"
+        DOCKER_NAME="${DOCKER_NAME}-${IMAGE_ARCH}_${BUILDKITE_TAG}"
+        ;;
+    "miner")
+        echo "Doing a miner image build for ${IMAGE_ARCH}"
+        if [[ "$IMAGE_ARCH" == "arm64" ]]; then
+            ERLANG_IMAGE_SOURCE="arm64v8"
+        fi
+        BASE_IMAGE="${ERLANG_IMAGE_SOURCE}:${ERLANG_IMAGE}"
+        DOCKER_BUILD_ARGS="--build-arg EXTRA_BUILD_APK_PACKAGES=apk-tools --build-arg EXTRA_RUNNER_APK_PACKAGES=apk-tools --build-arg BUILDER_IMAGE=${BASE_IMAGE} --build-arg RUNNER_IMAGE=${BASE_IMAGE} --build-arg REBAR_BUILD_TARGET=docker ${DOCKER_BUILD_ARGS}"
+        DOCKER_NAME="${DOCKER_NAME}-${IMAGE_ARCH}_${BUILDKITE_TAG}"
+        ;;
+    *)
+        echo "I don't know how to do a build for ${BUILD_TYPE}"
+        exit 1
+        ;;
+esac
 
 docker login -u="team-helium+buildkite" -p="${QUAY_BUILDKITE_PASSWORD}" ${REGISTRY_HOST}
-docker build -t helium:$DOCKER_NAME -f "${DOCKERFILE_NAME}" .
+docker build "$DOCKER_BUILD_ARGS" -t "helium:${DOCKER_NAME}" .
 docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:$DOCKER_NAME"
 docker push "$MINER_REGISTRY_NAME:$DOCKER_NAME"
-# only update latest if github tag contains `_GA`
-if [[ "$BUILDKITE_TAG" =~ _GA$ ]]; then
-    docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:latest-${IMAGE_ARCH}"
-    docker push "$MINER_REGISTRY_NAME:latest-${IMAGE_ARCH}"
-fi
+
+case "$BUILD_TYPE" in
+    "val")
+        LATEST_TAG="$MINER_REGISTRY_NAME:latest-val-${IMAGE_ARCH}"
+        docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:$LATEST_TAG"
+        docker push "$MINER_REGISTRY_NAME:$LATEST_TAG"
+        ;;
+    "validator")
+        # do nothing - for now
+        ;;
+    "miner")
+        # only update latest if github tag contains `_GA`
+        if [[ "$BUILDKITE_TAG" =~ _GA$ ]]; then
+            docker tag helium:$DOCKER_NAME "$MINER_REGISTRY_NAME:$LATEST_TAG"
+            docker push "$MINER_REGISTRY_NAME:$LATEST_TAG"
+        fi
+        ;;
+esac
