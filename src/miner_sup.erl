7c08c488e87e6a3739bae8d069fd06ee6593472e
andymck <andrewb.mckenzie@gmail.com>
2022-01-04 16:54:24 +0000

    put network setting back in for libp2p crypto

--- src/miner_sup.erl
+++ src/miner_sup.erl
@@ -47,6 +47,9 @@ init(_Args) ->
                 },
 
     BaseDir = application:get_env(blockchain, base_dir, "data"),
+
+    ok = libp2p_crypto:set_network(application:get_env(miner, network, mainnet)),
+
     case application:get_env(blockchain, key, undefined) of
         undefined ->
             #{ pubkey := PublicKey,
