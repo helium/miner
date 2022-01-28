03cb7bfc85e5cd5cfd9fc7b7633378dea397090c
PaulVMo <paul.moceri@gmail.com>
2022-01-28 08:53:48 -0600

    add genesis, run as helium, auto-start, move files if existing

--- .buildkite/scripts/make_deb.sh
+++ .buildkite/scripts/make_deb.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+BUILD_NET="${BUILD_NET:-mainnet}"
+
+
 VERSION=$(echo $VERSION_TAG | sed -e 's,validator,,')
 DIAGNOSTIC=1 ./rebar3 as validator release -n miner -v ${VERSION} || ./rebar3 as validator release -n miner -v ${VERSION}
+
+wget -O /tmp/genesis https://snapshots.helium.wtf/genesis.${BUILD_NET}
 
 fpm -n validator \
     -v "${VERSION}" \
@@ -11,6 +16,22 @@ fpm -n validator \
     -t deb \
     --depends libssl1.1 \
     --depends libsodium23 \
+    --depends libncurses5 \
+    --depends dbus \
+    --depends libstdc++6 \
     --deb-systemd deb/miner.service \
+    --before-install deb/before_install.sh \
+    --after-install deb/after_install.sh \
+    --after-remove deb/after_remove.sh \
+    --before-upgrade deb/before_upgrade.sh \
+    --after-upgrade deb/after_upgrade.sh \
     --deb-no-default-config-files \
-    _build/validator/rel/=/var/helium
+    --deb-systemd-enable \
+    --deb-systemd-auto-start \
+    --deb-systemd-restart-after-upgrade \
+    --deb-user helium \
+    --deb-group helium \
+    _build/validator/rel/=/opt \
+    /tmp/genesis=/opt/miner/update/genesis
+
+rm -f /tmp/genesis
---
+++ deb/before_upgrade.sh
@@ -0,0 +1,2 @@
+# add system user for file ownership and systemd user, if not exists
+useradd --system helium || true
