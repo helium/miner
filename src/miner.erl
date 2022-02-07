c7dc87a57e34863bd166b1de7bc42577f59683d4
Evan Vigil-McClanahan <evan@helium.com>
2022-02-07 10:50:45 -0800

    start rollup for 1.7 release

--- src/miner.erl
+++ src/miner.erl
@@ -322,7 +322,7 @@ remove_consensus() ->
 version() ->
     %% format:
     %% MMMmmmPPPP
-       0010060005.
+       0010070000.
 
 %% ------------------------------------------------------------------
 %% gen_server
@@ -362,10 +362,9 @@ handle_call({create_block, Metadata, Txns, HBBFTRound}, _From,
 handle_call(keys, _From, State) ->
     {reply, {ok, State#state.swarm_keys}, State};
 handle_call(reset_late_block_timer, _From, State) ->
-    %% we do this when the group thinks that it's agreed on a new round, so set the timer extra long
     erlang:cancel_timer(State#state.late_block_timer),
     LateBlockTimeout = application:get_env(miner, late_block_timeout_seconds, 120),
-    LateTimer = erlang:send_after((LateBlockTimeout * 2) * 1000, self(), late_block_timeout),
+    LateTimer = erlang:send_after(LateBlockTimeout * 1000, self(), late_block_timeout),
 
     {reply, ok, State#state{late_block_timer = LateTimer}};
 handle_call(_Msg, _From, State) ->
