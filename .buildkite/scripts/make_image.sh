1e3ec1b22dc9ab7f5eecfdcbaa6ba8b4aeca4d4f
Jay Kickliter <jay@kickliter.com>
2022-03-01 14:10:51 -0700

    Build and deploy to testnet quays via testnet_ prefix git tag

--- .buildkite/scripts/make_image.sh
+++ .buildkite/scripts/make_image.sh
@@ -24,41 +24,44 @@ if [[ "$IMAGE_ARCH" == "arm64" ]]; then
     RUN_IMAGE="arm64v8/$RUN_IMAGE"
 fi
 
-VERSION=$(echo $VERSION_TAG | sed -e "s/$BUILD_TYPE//")
-DOCKER_BUILD_ARGS="--build-arg VERSION=$VERSION --build-arg BUILD_NET=$BUILD_NET"
+VERSION=$(echo $VERSION_TAG | sed "s/^${BUILD_NET}_//" | sed "s/^${BUILD_TYPE}//")
+DOCKER_BUILD_ARGS="--build-arg BUILDER_IMAGE=$BUILD_IMAGE --build-arg RUNNER_IMAGE=$RUN_IMAGE --build-arg VERSION=$VERSION --build-arg BUILD_NET=$BUILD_NET"
 
 if [[ ! $TEST_BUILD -eq "0" ]]; then
     REGISTRY_NAME="test-builds"
     DOCKER_BUILD_ARGS="--build-arg REBAR_DIAGNOSTIC=1 $DOCKER_BUILD_ARGS"
 fi
-MINER_REGISTRY_NAME="$REGISTRY_HOST/$REGISTRY_ORG/$REGISTRY_NAME"
+FULL_REGISTRY_NAME="$REGISTRY_HOST/$REGISTRY_ORG/$REGISTRY_NAME"
 
-case "$BUILD_TYPE" in
-    "val")
-        echo "Doing a testnet validator image build for ${IMAGE_ARCH}"
-        DOCKER_BUILD_ARGS="--build-arg BUILDER_IMAGE=$BUILD_IMAGE --build-arg RUNNER_IMAGE=$RUN_IMAGE --build-arg REBAR_BUILD_TARGET=docker_testval $DOCKER_BUILD_ARGS"
-        BASE_DOCKER_NAME="validator"
-        DOCKER_NAME="${BASE_DOCKER_NAME}-${IMAGE_ARCH}_testnet_${VERSION}"
-        LATEST_TAG="latest-val-${IMAGE_ARCH}"
-        ;;
-    "validator")
+case "${BUILD_TYPE}-$BUILD_NET" in
+    "validator-mainnet")
         echo "Doing a mainnet validator image build for $IMAGE_ARCH"
-        DOCKER_BUILD_ARGS="--build-arg BUILDER_IMAGE=${BUILD_IMAGE} --build-arg RUNNER_IMAGE=${RUN_IMAGE} --build-arg REBAR_BUILD_TARGET=docker_val ${DOCKER_BUILD_ARGS}"
+        DOCKER_BUILD_ARGS="--build-arg REBAR_BUILD_TARGET=docker_val $DOCKER_BUILD_ARGS"
         BASE_DOCKER_NAME="validator"
         DOCKER_NAME="${BASE_DOCKER_NAME}-${IMAGE_ARCH}_${VERSION}"
         ;;
-    "miner")
+    "validator-testnet")
+        echo "Doing a testnet validator image build for ${IMAGE_ARCH}"
+        DOCKER_BUILD_ARGS="--build-arg REBAR_BUILD_TARGET=docker_testval $DOCKER_BUILD_ARGS"
+        BASE_DOCKER_NAME="validator"
+        DOCKER_NAME="${BASE_DOCKER_NAME}-${IMAGE_ARCH}_${VERSION}"
+        ;;
+    "miner-mainnet")
         echo "Doing a miner image build for ${IMAGE_ARCH}"
-        DOCKER_BUILD_ARGS="--build-arg EXTRA_BUILD_APK_PACKAGES=apk-tools --build-arg EXTRA_RUNNER_APK_PACKAGES=apk-tools --build-arg BUILDER_IMAGE=${BUILD_IMAGE} --build-arg RUNNER_IMAGE=${RUN_IMAGE} --build-arg REBAR_BUILD_TARGET=docker ${DOCKER_BUILD_ARGS}"
+        DOCKER_BUILD_ARGS="--build-arg EXTRA_BUILD_APK_PACKAGES=apk-tools --build-arg EXTRA_RUNNER_APK_PACKAGES=apk-tools --build-arg REBAR_BUILD_TARGET=docker $DOCKER_BUILD_ARGS"
         BASE_DOCKER_NAME=$(basename $(pwd))
         DOCKER_NAME="${BASE_DOCKER_NAME}-${IMAGE_ARCH}_${VERSION}"
         ;;
-    *)
-        echo "I don't know how to do a build for ${BUILD_TYPE}"
+    "miner-testnet")
+        echo "Doing a testnet miner image build for ${IMAGE_ARCH}"
+        DOCKER_BUILD_ARGS="--build-arg EXTRA_BUILD_APK_PACKAGES=apk-tools --build-arg EXTRA_RUNNER_APK_PACKAGES=apk-tools --build-arg REBAR_BUILD_TARGET=docker_testminer $DOCKER_BUILD_ARGS"
+        BASE_DOCKER_NAME=$(basename $(pwd))
+        DOCKER_NAME="${BASE_DOCKER_NAME}-${IMAGE_ARCH}_${VERSION}"
+        ;;    *)
+        echo "I don't know how to build ${BUILD_TYPE}-$BUILD_NET"
         exit 1
         ;;
 esac
-
 
 if [[ ! $TEST_BUILD ]]; then
     docker login -u="${REGISTRY_ORG}+buildkite" -p="${QUAY_BUILDKITE_PASSWORD}" ${REGISTRY_HOST}
@@ -82,13 +85,13 @@ if [[ "$BUILD_TYPE" == "miner" ]]; then
         UPDATE_TAG="${UPDATE_TAG}-${IMAGE_ARCH}"
         SEMVER_TAG=$(echo "$DOCKER_NAME" | sed -e 's/_GA//' -e 's/_alpha//' -e 's/_beta//')
 
-        docker pull "$MINER_REGISTRY_NAME:$SEMVER_TAG"
-        docker tag "$MINER_REGISTRY_NAME:$SEMVER_TAG" "$MINER_REGISTRY_NAME:$UPDATE_TAG"
-        docker push "$MINER_REGISTRY_NAME:$UPDATE_TAG"
+        docker pull "$FULL_REGISTRY_NAME:$SEMVER_TAG"
+        docker tag "$FULL_REGISTRY_NAME:$SEMVER_TAG" "$FULL_REGISTRY_NAME:$UPDATE_TAG"
+        docker push "$FULL_REGISTRY_NAME:$UPDATE_TAG"
 
         if [[ "$VERSION_TAG" =~ _GA$ ]]; then
-            docker tag "$MINER_REGISTRY_NAME:$SEMVER_TAG" "$MINER_REGISTRY_NAME:$DOCKER_NAME"
-            docker push "$MINER_REGISTRY_NAME:$DOCKER_NAME"
+            docker tag "$FULL_REGISTRY_NAME:$SEMVER_TAG" "$FULL_REGISTRY_NAME:$DOCKER_NAME"
+            docker push "$FULL_REGISTRY_NAME:$DOCKER_NAME"
         fi
 
         exit $?
@@ -96,5 +99,11 @@ if [[ "$BUILD_TYPE" == "miner" ]]; then
 fi
 
 docker build $DOCKER_BUILD_ARGS -t "helium:${DOCKER_NAME}" .
-docker tag "helium:$DOCKER_NAME" "$MINER_REGISTRY_NAME:$DOCKER_NAME"
-docker push "$MINER_REGISTRY_NAME:$DOCKER_NAME"
+docker tag "helium:$DOCKER_NAME" "$FULL_REGISTRY_NAME:$DOCKER_NAME"
+docker push "$FULL_REGISTRY_NAME:$DOCKER_NAME"
+
+if [[ "$BUILD_NET" == "testnet" ]]; then
+    LATEST_TAG="latest-$IMAGE_ARCH"
+    docker tag "$FULL_REGISTRY_NAME:$DOCKER_NAME" "$FULL_REGISTRY_NAME:$LATEST_TAG"
+    docker push "$FULL_REGISTRY_NAME:$LATEST_TAG"
+fi
