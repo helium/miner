216f1e4dcf2c70dbd484a481f93ef505fd38a937
jeffgrunewald <jeff@grunewalddesign.com>
2022-04-04 12:19:23 -0400

    sort v2 receipts as with v1 receipts

--- src/handlers/miner_hbbft_handler.erl
+++ src/handlers/miner_hbbft_handler.erl
@@ -246,14 +246,16 @@ handle_command({txn, Txn}, State=#state{hbbft=HBBFT}) ->
             %% sort non poc transactions first
             Comparator = case blockchain_txn:type(Txn) of
                              X when X == blockchain_txn_poc_request_v1 orelse
-                                    X == blockchain_txn_poc_receipts_v1 ->
+                                    X == blockchain_txn_poc_receipts_v1 orelse
+                                    X == blockchain_txn_poc_receipts_v2 ->
                                  fun(_) -> true end;
                              _ ->
                                  fun(OtherSerializedTxn) ->
                                         OtherTxn = blockchain_txn:deserialize(OtherSerializedTxn),
                                         case blockchain_txn:type(OtherTxn) of
                                             X when X == blockchain_txn_poc_request_v1 orelse
-                                                   X == blockchain_txn_poc_receipts_v1 ->
+                                                   X == blockchain_txn_poc_receipts_v1 orelse
+                                                   X == blockchain_txn_poc_receipts_v2 ->
                                                 false;
                                             _ ->
                                                 true
