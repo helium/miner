845b5b8215d30a1c1209c0a94b95cee150c53bd7
Jade Allen <jadeallenx@outlook.com>
2022-04-18 11:47:59 -0500

    Also make hotfix directory during upgrade

--- deb/before_upgrade.sh
+++ deb/before_upgrade.sh
@@ -1,2 +1,7 @@
 # add system user for file ownership and systemd user, if not exists
 useradd --system helium || true
+
+# add hotfix directory if it doesn't already exist
+if [ ! -d /opt/miner/hotfix ]; then
+    mkdir -p /opt/miner/hotfix
+fi
