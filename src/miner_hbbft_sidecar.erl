d450502736b37c5b7a63a13b93676ba7977f80ac
Andrew Thompson <andrew@hijacked.us>
2023-01-17 08:27:42 -0800

    Flag miner process as doing speculative absorbs as well

--- src/miner_hbbft_sidecar.erl
+++ src/miner_hbbft_sidecar.erl
@@ -98,6 +98,7 @@ handle_txn(update = _RequestType, Txn) ->
 init([]) ->
     init(#state{});
 init(State) ->
+    %% flag this process as doing speculative absorbs
     put('__speculative_absorb', true),
     case blockchain_worker:blockchain() of
         undefined ->
